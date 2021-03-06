164142655f80b12e34807f13efa3d5f85b653ea4
Jaeho Jeong <ywm2004@gmail.com>
2021-03-06 18:18:57 +0900

    # [ DB 수정 ]

--- 정재호_애플리케이션테스트수행/SQL/Exam.sql
+++ 정재호_애플리케이션테스트수행/SQL/Exam.sql
@@ -18,33 +18,16 @@ create table Board(
     bdViews integer    
 );
 
+insert into board (bdNum, bdTitle, bdAuthor, bdCreateDate, bdViews) values(null, "게시글", "정재호", now(), 0);
+
 delimiter $$
 drop procedure if exists loopInsert $$
 create procedure loopInsert()
 begin
 declare i int default 1;
-while(i < 1000000) do
-insert into board (bdNum, bdTitle, bdAuthor, bdCreateDate, bdViews) values (null, concat('게시글 쓰기 ', i), "정재호", now(), null),
-																		   (null, concat('게시글 쓰기 ', i + 1), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 2), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 3), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 4), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 5), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 6), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 7), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 8), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 9), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 10), "정재호", now(), null),
-																		   (null, concat('게시글 쓰기 ', i + 11), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 12), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 13), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 14), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 15), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 16), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 17), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 18), "정재호", now(), null),
-                                                                           (null, concat('게시글 쓰기 ', i + 19), "정재호", now(), null);
-set i = i + 20;
+while(i < 21) do
+insert into board (bdTitle, bdAuthor, bdCreateDate, bdViews) (select bdTitle, bdAuthor, bdCreateDate, bdViews from board);
+set i = i + 1;
 end while;
 end $$
 
